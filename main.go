995dd01c15b74c2a6960f9bffe9850ada9d28335
Sebastian Baumann (Baschte83) <bauman21@hm.edu>
2019-06-03 22:29:32 +0200

    Writing a parallel approach of the dynamic algorithm and writing a benchmark which compares execution time of recursive and parallel dynamic algorithm

--- main.go
+++ main.go
@@ -1,21 +1,24 @@
 package main
 
 import (
-		"fmt"
-		"sort"
-		"math"
-		"errors"
-		"strconv"
+	"errors"
+	"fmt"
+	"math"
+	"sort"
+	"strconv"
+	"sync"
 )
 
+var wg sync.WaitGroup
+
 type item struct {
-	name   string
-	volume, worth  int
+	name          string
+	volume, worth int
 }
 
 type knapsack struct {
-	items       []item
-	totalWorth, currentItemsVolume, maxVolume  int
+	items                                     []item
+	totalWorth, currentItemsVolume, maxVolume int
 }
 
 func (k *knapsack) addItem(i item) error {
@@ -38,45 +41,113 @@ func greedy(is []item, k *knapsack) {
 }
 
 func checkItem(k *knapsack, i int, j int, is []item, matrix [][]int) {
-    if i <= 0 || j <= 0 {
-        return
-    }
+	if i <= 0 || j <= 0 {
+		return
+	}
 
-    pick := matrix[i][j]
-    if pick != matrix[i-1][j] {
-        k.addItem(is[i-1])
-        checkItem(k, i-1, j-is[i-1].volume, is, matrix)
-    } else {
-        checkItem(k, i-1, j, is, matrix)
-    }
+	pick := matrix[i][j]
+	if pick != matrix[i-1][j] {
+		k.addItem(is[i-1])
+		checkItem(k, i-1, j-is[i-1].volume, is, matrix)
+	} else {
+		checkItem(k, i-1, j, is, matrix)
+	}
+}
+
+// CheckItem for concurrent computation
+func checkItemParallel(k *knapsack, i int, j int, is []item, matrix [][]int) {
+	if i <= 0 || j <= 0 {
+		return
+	}
+
+	wg.Add(1) // for starting a new go routine
+
+	pick := matrix[i][j]
+	if pick != matrix[i-1][j] {
+		k.addItem(is[i-1])
+		go checkItemParallel(k, i-1, j-is[i-1].volume, is, matrix)
+	} else {
+		go checkItemParallel(k, i-1, j, is, matrix)
+	}
+
+	wg.Done()
 }
 
 func dynamic(is []item, k *knapsack) *knapsack {
-    numItems := len(is)          // number of items in knapsack
-    capacity := k.maxVolume
+	numItems := len(is) // number of items in knapsack
+	capacity := k.maxVolume
 
-    // create the empty matrix
-    matrix := make([][]int, numItems+1) // items
-    for i := range matrix {
-        matrix[i] = make([]int, capacity+1) // volumes
-    }
+	// create the empty matrix
+	matrix := make([][]int, numItems+1) // items
+	for i := range matrix {
+		matrix[i] = make([]int, capacity+1) // volumes
+	}
 
-    for i := 1; i <= numItems; i++ {
-        for j := 1; j <= capacity; j++ {
-            if is[i-1].volume <= j {
-                valueOne := float64(matrix[i-1][j])
-                valueTwo := float64(is[i-1].worth + matrix[i-1][j-is[i-1].volume])
-                matrix[i][j] = int(math.Max(valueOne, valueTwo))
-            } else {
-                matrix[i][j] = matrix[i-1][j]
-            }
-        }
-    }
-    checkItem(k, numItems, capacity, is, matrix)
-    k.totalWorth = matrix[numItems][capacity]
-    //k.totalWeight = k.currentItemsVolume
+	for i := 1; i <= numItems; i++ {
+		for j := 1; j <= capacity; j++ {
+			if is[i-1].volume <= j {
+				valueOne := float64(matrix[i-1][j])
+				valueTwo := float64(is[i-1].worth + matrix[i-1][j-is[i-1].volume])
+				matrix[i][j] = int(math.Max(valueOne, valueTwo))
+			} else {
+				matrix[i][j] = matrix[i-1][j]
+			}
+		}
+	}
 
-    return k
+	// Printing the matrix just to know what happens
+	// fmt.Println("Matrix is:")
+	// printMatrix(matrix, capacity)
+
+	checkItem(k, numItems, capacity, is, matrix)
+	k.totalWorth = matrix[numItems][capacity]
+	//k.totalWeight = k.currentItemsVolume
+
+	return k
+}
+
+func dynamicParallel(is []item, k *knapsack) *knapsack {
+	numItems := len(is) // number of items in knapsack
+	capacity := k.maxVolume
+
+	// create the empty matrix
+	matrix := make([][]int, numItems+1) // items
+	for i := range matrix {
+		matrix[i] = make([]int, capacity+1) // volumes
+	}
+
+	for i := 1; i <= numItems; i++ {
+		for j := 1; j <= capacity; j++ {
+			if is[i-1].volume <= j {
+				valueOne := float64(matrix[i-1][j])
+				valueTwo := float64(is[i-1].worth + matrix[i-1][j-is[i-1].volume])
+				matrix[i][j] = int(math.Max(valueOne, valueTwo))
+			} else {
+				matrix[i][j] = matrix[i-1][j]
+			}
+		}
+	}
+
+	checkItemParallel(k, numItems, capacity, is, matrix)
+	k.totalWorth = matrix[numItems][capacity]
+
+	// Waiting for all go routines to end
+	wg.Wait()
+
+	return k
+}
+
+// Function to easily print a matrix
+func printMatrix(mat [][]int, length int) {
+	for i, outer := range mat {
+		for j := range outer {
+			if j == length {
+				fmt.Printf("%v\n", mat[i][j])
+			} else {
+				fmt.Printf("%v\t", mat[i][j])
+			}
+		}
+	}
 }
 
 func main() {
@@ -96,6 +167,8 @@ func main() {
 
 	kd := knapsack{items: make([]item, 0), totalWorth: 0, currentItemsVolume: 0, maxVolume: 10}
 
+	kdPar := knapsack{items: make([]item, 0), totalWorth: 0, currentItemsVolume: 0, maxVolume: 10}
+
 	greedy(initItems, &kg)
 	fmt.Println("Greedy Algorithm:")
 	resultg := ""
@@ -106,14 +179,36 @@ func main() {
 	fmt.Println(resultg)
 	fmt.Println("Total Worth: " + strconv.Itoa(resultgWorth))
 	fmt.Println("Total Volume: " + strconv.Itoa(kg.currentItemsVolume))
+
+	// Dynamic computation starts here:
 	dynamic(initItems, &kd)
+
+	fmt.Println()
 	fmt.Println("Dynamic Algorithm:")
 	resultd := ""
 	resultdWorth := kd.totalWorth
+
 	for _, it := range kd.items {
 		resultd += it.name + " "
 	}
+
 	fmt.Println(resultd)
 	fmt.Println("Total Worth: " + strconv.Itoa(resultdWorth))
 	fmt.Println("Total Volume: " + strconv.Itoa(kd.currentItemsVolume))
+
+	// Dynamic computation with parallel checkItems function:
+	dynamic(initItems, &kdPar)
+
+	fmt.Println()
+	fmt.Println("Dynamic Algorithm in parallel:")
+	resultd = ""
+	resultdWorth = kdPar.totalWorth
+
+	for _, it := range kdPar.items {
+		resultd += it.name + " "
+	}
+
+	fmt.Println(resultd)
+	fmt.Println("Total Worth parallel: " + strconv.Itoa(resultdWorth))
+	fmt.Println("Total Volume parallel: " + strconv.Itoa(kdPar.currentItemsVolume))
 }
---
+++ main_test.go
@@ -0,0 +1,48 @@
+// Running this Benchmark with (within the same directory as main.go):
+// $ go test -bench=.
+
+package main
+
+import (
+	"testing"
+)
+
+func BenchmarkDynamic(b *testing.B) {
+	initItems := []item{
+		item{name: "Apple", volume: 3, worth: 30},
+		item{name: "Apple", volume: 3, worth: 30},
+		item{name: "Orange", volume: 4, worth: 30},
+		item{name: "Orange", volume: 4, worth: 30},
+		item{name: "Pencil", volume: 1, worth: 10},
+		item{name: "Pencil", volume: 1, worth: 10},
+		item{name: "Pencil", volume: 1, worth: 10},
+		item{name: "Mirror", volume: 5, worth: 40},
+		item{name: "Mirror", volume: 5, worth: 40},
+	}
+
+	kd := knapsack{items: make([]item, 0), totalWorth: 0, currentItemsVolume: 0, maxVolume: 10}
+
+	for n := 0; n < b.N; n++ {
+		dynamic(initItems, &kd)
+	}
+}
+
+func BenchmarkDynamicParallel(b *testing.B) {
+	initItems := []item{
+		item{name: "Apple", volume: 3, worth: 30},
+		item{name: "Apple", volume: 3, worth: 30},
+		item{name: "Orange", volume: 4, worth: 30},
+		item{name: "Orange", volume: 4, worth: 30},
+		item{name: "Pencil", volume: 1, worth: 10},
+		item{name: "Pencil", volume: 1, worth: 10},
+		item{name: "Pencil", volume: 1, worth: 10},
+		item{name: "Mirror", volume: 5, worth: 40},
+		item{name: "Mirror", volume: 5, worth: 40},
+	}
+
+	kd := knapsack{items: make([]item, 0), totalWorth: 0, currentItemsVolume: 0, maxVolume: 10}
+
+	for n := 0; n < b.N; n++ {
+		dynamicParallel(initItems, &kd)
+	}
+}
